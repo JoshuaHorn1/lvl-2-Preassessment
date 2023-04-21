fcebc2977752d4a532eab142c2fda9d55bcd38af
JoshuaHorn1 <98923400+JoshuaHorn1@users.noreply.github.com>
2023-04-21 14:35:12 +1200

    Update 9

--- 00_BM_base_v5.py
+++ 00_BM_base_v5.py
@@ -1,6 +1,8 @@
 """Burger Menu Combo Base/Main Program file - Version 4
 Components added after they have been created and tested
-Added the Search button function, with delete and change components incorporated"""
+Added add combo button
+Removed 'delete' option from main menu and help menu
+Updated text in help menu"""
 
 # imports...
 import easygui as eg  # importing easygui as 'eg' to save time later (more efficient code)
@@ -33,20 +35,17 @@ def mainmenu(proceed):  # a function containing the code for the main menu
         while proceed != "Exit":  # create a while loop to incorporate functions later on
             if proceed == "Search":
                 search_button()
-                proceed = eg.buttonbox("How would you like to proceed?", "MAIN MENU", choices=("Search", "Add", "Delete", "Help", "Exit"))
+                proceed = eg.buttonbox("How would you like to proceed?", "MAIN MENU", choices=("Search", "Add", "Help", "Exit"))
             elif proceed == "Add":
-                proceed = eg.buttonbox("How would you like to proceed?", "MAIN MENU", choices=("Search", "Add", "Delete", "Help", "Exit"))
-            elif proceed == "Delete":
-                proceed = eg.buttonbox("How would you like to proceed?", "MAIN MENU", choices=("Search", "Add", "Delete", "Help", "Exit"))
+                addcombo()
+                proceed = eg.buttonbox("How would you like to proceed?", "MAIN MENU", choices=("Search", "Add", "Help", "Exit"))
             elif proceed == "Help":
                 helpbutton()
-                proceed = eg.buttonbox("How would you like to proceed?", "MAIN MENU", choices=("Search", "Add", "Delete", "Help", "Exit"))
-            else:
-                proceed = eg.buttonbox("How would you like to proceed?", "Menu Choices", choices=("Search", "Add", "Delete", "Help", "Exit"))
+                proceed = eg.buttonbox("How would you like to proceed?", "MAIN MENU", choices=("Search", "Add", "Help", "Exit"))
         quit = eg.buttonbox("Are you sure you want to quit? All progress will be lost!!", "Quit?", choices=("Yes - Quit", "No - Cancel"))
         if quit == "No - Cancel":
             quit = ""
-            proceed = eg.buttonbox("How would you like to proceed?", "Menu Choices", choices=("Search", "Add", "Delete", "Help", "Exit"))
+            proceed = eg.buttonbox("How would you like to proceed?", "Menu Choices", choices=("Search", "Add", "Help", "Exit"))
     eg.msgbox("Thank you for using this Menu Combo Program!!", "Goodbye!", ok_button=":D")
 
 
@@ -55,20 +54,15 @@ def helpbutton():  # A function containing the code for when a user interacts wi
     while help_options != "Cancel":  # checks if the user wants to cancel, if not, loops
         help_options = eg.buttonbox("Sure! What do you need help with?\n"
                                     "(Click the Cancel button to return to the main menu).",
-                                    "HELP MENU", choices=("'Search'", "'Add'", "'Delete'",
-                                                          "'Help'", "'Exit'", "Cancel"))
+                                    "HELP MENU", choices=("'Search'", "'Add'", "'Help'", "'Exit'", "Cancel"))
         if help_options == "'Search'":  # depending on what button is selected, information about the button will be displayed
             eg.msgbox("This button allows you to search through the combo's currently stored in the database. If there is a "
                       "match for the combo name entered, it will display the combo, and then ask if you want to make any "
-                      "changes to it, or to cancel and return to the main menu.", "Search-Button Help", ok_button="Back")
+                      "changes to it, delete the combo, or to cancel and return to the main menu.", "Search-Button Help", ok_button="Back")
         elif help_options == "'Add'":
             eg.msgbox("This button creates a new combo to add to the menu. After entering the details for the new combo, "
                       "it will ask if you want to make any changes, add it to the menu, or cancel after/during the process.",
                       "Add-Button Help", ok_button="Back")
-        elif help_options == "'Delete'":
-            eg.msgbox("This button allows you to delete a combo from the list that currently exits. You will enter the name "
-                      "of a combo, and if it exists, you will be asked to confirm that you want to delete it, or cancel "
-                      "after/during the process.", "Delete-Button Help", ok_button="Back")
         elif help_options == "'Help'":
             eg.msgbox("This button displays the Help Menu, from which you can view small, informative tips about all of the "
                       "buttons on the main menu - such as this one.", "Help-Button Help", ok_button="Back")
@@ -128,6 +122,36 @@ def search_button():
                 query = eg.enterbox("Enter the combo name to search for:", "Enter Query").upper()
 
 
+def addcombo():
+    proceed = "Change"
+    while proceed != "Cancel":
+        if proceed == "Change":
+                combo_items = {}
+                num_items = eg.buttonbox("How many items are in this combo?", "Num Items", choices=(1, 2, 3, "Cancel"))
+                if num_items != "Cancel":
+                    name = eg.enterbox("What is the name of this combo?").upper()
+                    while num_items != 0:
+                        item_name = eg.enterbox("What is the name of an item in the combo?", "Combo Item Name").capitalize()
+                        item_price = eg.enterbox("How much does this item cost? ($)", "Combo Item Price")
+                        combo_items.update({item_name: item_price})
+                        num_items -= 1
+                    full_new_combo = {name: combo_items}
+                    items = "\n".join([f"{item}: {price}" for item, price in combo_items.items()])
+                    proceed = eg.buttonbox(f"Here is the combo, {name.capitalize()}:\n"
+                                           f"{items}\n"
+                                           f"\n" 
+                                           f"What would you like to do with it?", "Combo Created", choices=("Use", "Change", "Cancel"))
+                else:
+                    return
+                if proceed == "Use":
+                    proceed = eg.buttonbox(f"Please confirm you want to add the combo '{name.capitalize()}' to the menu?",
+                                            choices=("Confirm", "Cancel"))
+                    if proceed == "Confirm":
+                        combos.update(full_new_combo)
+                        eg.msgbox(f"Your changes have been saved, and the combo '{name.capitalize()}' has been added to the menu.", "Combo Added")
+                        return
+
+
 # Main code...
 mainmenu(eg.buttonbox("Hello! Welcome to the Ultimate Burger Combo Menu!\n"  # calls on the main menu function using a player input
-                      "What would you like to do?", "MAIN MENU", choices=("Search", "Add", "Delete", "Help", "Exit")))
+                      "What would you like to do?", "MAIN MENU", choices=("Search", "Add", "Help", "Exit")))
---
+++ 06_add_combo_v3.py
@@ -0,0 +1,60 @@
+"""Add combo - Version 3
+Converted into function
+Bug fixes"""
+
+# Imports...
+import easygui as eg
+
+
+# Dictionaries/Lists...
+combos = {
+    "VALUE": {
+        "Beef burger": 5.69,
+        "Fizzy drink": 1,
+        "Fries": 1
+    },
+    "CHEEZY": {
+        "Cheeseburger": 6.69,
+        "Fizzy drink": 1.00,
+        "Fries": 1.00
+    },
+    "SUPER": {
+        "Cheeseburger": 6.69,
+        "Large fries": 2.00,
+        "Smoothie": 2.00
+    }
+}
+
+
+# Functions...
+def addcombo():
+    proceed = "Change"
+    while proceed != "Cancel":
+        if proceed == "Change":
+                combo_items = {}
+                num_items = eg.buttonbox("How many items are in this combo?", "Num Items", choices=(1, 2, 3, "Cancel"))
+                if num_items != "Cancel":
+                    name = eg.enterbox("What is the name of this combo?").upper()
+                    while num_items != 0:
+                        item_name = eg.enterbox("What is the name of an item in the combo?", "Combo Item Name").capitalize()
+                        item_price = eg.enterbox("How much does this item cost? ($)", "Combo Item Price")
+                        combo_items.update({item_name: item_price})
+                        num_items -= 1
+                    full_new_combo = {name: combo_items}
+                    items = "\n".join([f"{item}: {price}" for item, price in combo_items.items()])
+                    proceed = eg.buttonbox(f"Here is the combo, {name.capitalize()}:\n"
+                                           f"{items}\n"
+                                           f"\n" 
+                                           f"What would you like to do with it?", "Combo Created", choices=("Use", "Change", "Cancel"))
+                else:
+                    return
+                if proceed == "Use":
+                    proceed = eg.buttonbox(f"Please confirm you want to add the combo '{name.capitalize()}' to the menu?",
+                                            choices=("Confirm", "Cancel"))
+                    if proceed == "Confirm":
+                        combos.update(full_new_combo)
+                        eg.msgbox(f"Your changes have been saved, and the combo '{name.capitalize()}' has been added to the menu.", "Combo Added")
+                        return
+
+# Main code...
+addcombo()
